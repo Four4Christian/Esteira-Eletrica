6f10d5b6165b6baa8989625d4f3b9364cc44345a
Christian Batista <41270650+Four4Christian@users.noreply.github.com>
2025-09-22 11:05:42 +0000

    main

--- main.py
+++ main.py
@@ -6,18 +6,47 @@ P2 = Velocidade aumenta 2x e diminui  Vel1 Vel2 Vel3 Vel2 Vel1
 P3 = Velocidade alternada V1 V2 V3 V2 V3 V2 V1
 P4 = Distância - Quando chegar na distância máxima, a corrida acaba
 """
-#vel1 = vel2 = vel3 = tempo = None
+vel1 = vel2 = velmax = tempo = None
+
+
+def inicial():
+    m = int(input('Selecione o módulo: \n(1)P1 (2)P2 (3)P3 (4)P4 \n> '))
+
+    if m == 1:
+        P1()
+    elif m == 2:
+        P2()
+    elif m == 3:
+        P3()
+    elif m == 4:
+        P4()
+    else:
+        print(f'Opção Inválida "{m}"')
+        inicial()
+
+def consVel():
+    global velmax
+    try:
+        velmax = float(input('Insira Velocidade Máxima: '))
+        if velmax > 10:
+            velmax = 10
+    except ValueError:
+        print('Apenas Números')
+        consVel
+        
+
 def P1():
     global vel1
-    global vel2
+    global velmax
     global tempo 
-    
-    vel2 = int(input('Velocidade Máxima: '))
+
+    consVel()
     tempo = int(input('Insira o tempo em minutos: '))
 
-    vel1 = (vel2/2) + (vel2/3)
+    vel1 = (velmax/2) + (velmax/3)
 
     print(f'v1{vel1}')
-    print(f'Iniciando v{vel2} por {tempo}m')
+    print(f'Iniciando v{velmax} por {tempo}m')
 
-P1()
+
+inicial()
